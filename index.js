49d66f2420b783e2efc915da2606ecea196abb66
Jmassaro7 <122957849+Jmassaro7@users.noreply.github.com>
2023-01-19 11:05:30 -0500

    bot
    simple bot

---
+++ index.js
@@ -0,0 +1,59 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require("discord.js");
+const prefix = '>';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+    console.log("Bot is Online!");
+
+    client.user.setActivity("Intro Bot", {type: "WATCHING" });
+})
+
+client.on("messageCreate" , (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    //Message Array
+
+    const messageArray = message.content.split(" ");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+    //COMMANDS
+
+
+
+
+})
+
+if (command === 'test') {
+    message.channel.send("Bot is working!")
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("MTA2NTE0MTAzNTM5Njc4MDA5Mw.GZFbXB.jiiIaHRK-BvKJLXQ3FVavHo3FXpAsSn6NWuGHY")
